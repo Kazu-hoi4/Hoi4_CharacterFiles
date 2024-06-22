415bdaf998d78de7f632d551a973e03a71f56bb7
Kazu-Hoi4 <taka.railway.10032@gmail.com>
2024-06-22 21:25:36 +0900

    [Add] all_folder

---
+++ common/defines/flt_defines.lua
@@ -0,0 +1,28 @@
+NDefines.NCountry.BASE_MAX_COMMAND_POWER = 1000.0;
+NDefines.POLITICAL_POWER_LOWER_CAP = -1000;
+POLITICAL_POWER_UPPER_CAP = 5000;
+
+NDefines.NBuildings.MAX_BUILDING_LEVELS = 25;
+NDefines.NBuildings.MAX_SHARED_SLOTS = 50;
+
+NDefines.NMilitary.UNIT_LEADER_INITIAL_TRAIT_SLOT = { 2.0, 1.0, 1.0, 0.0, };
+NDefines.NMilitary.UNIT_LEADER_TRAIT_SLOT_PER_LEVEL = { 0.20, 0.20, 0.20, 0.02, };
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000;
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000;
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000;
+
+NDefines.NCharacter.OFFICER_CORP_HIGH_COMMAND_SLOTS_IN_MENU = 4;
+NDefines.NCharacter.SPECIALIST_ADVISOR_MIN_RANK = 40;
+NDefines.NCharacter.EXPERT_ADVISOR_MIN_RANK = 60;
+NDefines.NCharacter.GENIUS_ADVISOR_MIN_RANK = 80;
+NDefines.NCharacter.OFFICER_CORP_ADVISOR_ENTRIES_IN_MENU = {
+	"high_command",
+	"theorist",
+	"chief",
+	"army_chief",
+	"navy_chief",
+	"air_chief",
+	"minister_of_army",
+	"minister_of_navy",
+	"minister_of_air",
+};
---
+++ descriptor.mod
@@ -0,0 +1,20 @@
+version="1.0.0"
+tags={
+	"Historical"
+	"Map"
+	"National Focuses"
+	"Technologies"
+	"Events"
+	"Gameplay"
+	"Balance"
+	"Fixes"
+	"Ideologies"
+	"Military"
+}
+dependencies={
+	"Official Japanese Language fix"
+	"General Rank System"
+}
+name="Hoi4 LIKE Hoi2"
+supported_version="1.14.6"
+remote_file_id="3269686510"
